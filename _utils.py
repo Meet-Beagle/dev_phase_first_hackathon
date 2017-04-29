a3135260d1b3f33b5be1a74532717f2dae030290
George Dadunashvili <g.dadunashvili@lmu.de>
2017-04-29 18:05:03 +0200

    george: this file was copied from my master thesis project, and I neaded to remove some dependencies, for it to run

--- _utils.py
+++ _utils.py
@@ -6,48 +6,6 @@ import os
 
 # 3rd party module imports
 import numpy as np
-import pyximport
-
-# own Cython module imports
-pyximport.install(language_level=3,
-                  setup_args={"include_dirs":np.get_include()},
-                  reload_support=True)
-
-from genco.pyx_modules import dictator
-
-
-def _small_data_run(funct):
-
-    number = 18
-    n_iter = 20
-    sequence_length = 10
-    trans_mat = np.array([[0.4, 0.6], [0.4, 0.6]])
-    alphabet = np.array(['A', 'B'])
-    size_alphabet = np.alen(alphabet)
-    numeric_alphabet = np.arange(size_alphabet)
-    num_trans_dict = dictator(trans_mat, numeric_alphabet)
-
-    if funct.__name__ is 'sequential_dna_assembly':
-        return funct(sequence_length, num_trans_dict, size_alphabet,
-                     numeric_alphabet)
-
-    elif funct.__name__ == 'c_sequential_dna_assembly':
-        seed = np.empty(sequence_length, dtype=np.int_)
-        return funct(seed, sequence_length, num_trans_dict, size_alphabet,
-                     numeric_alphabet)
-
-    elif funct.__name__ is 'sequentially_assembled_dna_list':
-        return funct(number, sequence_length, trans_mat,
-                     size_alphabet, numeric_alphabet, string_output=False)
-
-    elif funct.__name__ == 'c_sequence_list_assembler':
-        return funct(number, n_iter, sequence_length, trans_mat,
-                     size_alphabet, alphabet, string_output=False)
-    else:
-        raise ValueError('unknown function was passed, only '
-                         'c_sequence_list_assembler, '
-                         'sequential_dna_assembly and '
-                         'sequentially_assembled_dna_list are supported')
 
 
 def _string_is_empty_or_spaces(string: str):

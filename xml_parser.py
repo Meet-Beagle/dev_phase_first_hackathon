ee88c6b9ef084a3397911ade6e63c7224ac1fb85
George Dadunashvili <g.dadunashvili@lmu.de>
2017-04-29 16:47:53 +0200

    initial version of xml/jason parser

---
+++ xml_parser.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+
+def basic_read():
+    tree = ET.parse('mock_data.xml', )
+    root = tree.getroot()
+    print(root.tag)
+    # print(dict(root))
+    for child in root:
+        print(child.tag, child.attrib)
+        print('    |')
+        for grand_child in child:
+            print('    |', 8*'-', grand_child.tag, grand_child.text)
+
+
